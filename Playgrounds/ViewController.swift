37840cc47f21e73955b13a76643682d329af1321
Edward Lay <edwardlay@edwards-mini.gateway>
2018-11-18 16:47:56 +1100

    Initial Commit

---
+++ Playgrounds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Playgrounds
+//
+//  Created by Edward Lay on 31/03/2015.
+//  Copyright (c) 2015 edtechl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
